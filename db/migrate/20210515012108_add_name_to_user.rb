f42a3f38a43da49f6efec90a274aac89c918283a
Leticia Mayumi <hashizumemay@gmail.com>
2021-05-15 16:07:14 -0300

    add fields to user, add links

--- db/migrate/20210515012108_add_name_to_user.rb
+++ db/migrate/20210515012108_add_name_to_user.rb
@@ -1,5 +1,6 @@
 class AddNameToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string, default: ''
+    add_column :users, :name, :string, null: false, default: ''
+    add_column :users, :phone, :string, null: false, default: ''
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,8 @@ ActiveRecord::Schema.define(version: 2021_05_15_174244) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", default: ""
+    t.string "name", default: "", null: false
+    t.string "phone", default: "", null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
