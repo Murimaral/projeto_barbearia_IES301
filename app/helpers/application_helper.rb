5aaa48c42e9ba6781512ac9e82870e1bba150871
Leticia Mayumi <hashizumemay@gmail.com>
2021-06-02 23:56:38 -0300

    layout improvements for pet profile and cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def species(species)
+    species == 'dog' ? 'Cachorro' : 'Gato'
+  end
+
+  def sex(sex)
+    sex == 'male' ? 'Macho' : 'Fêmea'
+  end
+
+  def status(status)
+    status == 'found' ? 'Resgatado' : 'Perdido'
+  end
+
+  def rescue_status(status)
+    strings = {
+      open: 'Aberto',
+      cancelled: 'Cancelado',
+      refused: 'Recusado',
+      accepted: 'Aceito'
+    }
+    strings[status.to_sym]
+  end
 end
