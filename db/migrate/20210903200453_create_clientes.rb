884b83c775390dcef6ed3e77d07d2e37f555f56e
Murimaral <murimaral@gmail.com>
2021-09-19 15:47:30 -0300

    Adicionado devise e criado o user. Os models funcionario e cliente foram apagados

--- db/migrate/20210903200453_create_clientes.rb
+++
@@ -1,9 +0,0 @@
-class CreateClientes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :clientes do |t|
-      t.string :nome
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_200453) do
+ActiveRecord::Schema.define(version: 2021_09_19_184249) do
 
-  create_table "clientes", force: :cascade do |t|
-    t.string "nome"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.integer "permission"
+    t.string "cpf"
+    t.date "birth"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "funcionarios", force: :cascade do |t|
-    t.string "nome"
-    t.string "cargo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
