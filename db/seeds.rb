230c68dc1781c9898cf39d242bf8d7bb1b7dc76e
lucas <linkbolto@gmail.com>
2021-12-01 01:40:34 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,50 @@
 if User.count.zero?
-  User.create(email: 'user@email.com', password: 'abc123', name: 'Olivia Lobo', phone: '(11) 987654321')
-  User.create(email: 'admin@barbershop.com', password: 'abc123', name: 'ConnectPlus', admin: true, phone: '(11) 912345678')
-  Customer.create(email: 'leticia@cta.com.br', name: 'Mayumi', phone: '(11) 123456789', details: 'oi descriçao')
-  Customer.create(email: 'lucas@cta.com.br', name: 'Lucas', phone: '(11) 931233213', details: 'primo da Adelaide')
+  User.create(email: 'admin@luzdomundo.com.br', password: 'abc123', name: 'Cristian de Lima Silva', admin: true, phone: '(11) 99999-9999')
+  User.create(email: 'atendente@luzdomundo.com.br', password: 'abc123', name: 'Olivia Lobo', phone: '(11) 98765-4321')
 
-  5.times do
-    Service.create(name: Faker::Job.title)
+  [
+    'Manicure',
+    'Pedicure',
+    'Corte feminino',
+    'Corte masculino',
+    'Corte máquina',
+    'Barba',
+    'Barba desenhada',
+    'Maquiagem',
+    'Luzes',
+    'Mechas',
+    'Alisamento',
+    'Progressiva',
+    'Coloração',
+    'Escova',
+    'Sobrancelhas'
+  ].each do |name|
+    Service.create(name: name)
   end
 
-  5.times do
-    Employee.create(
-      name: Faker::Name.unique.name,
-      phone: Faker::PhoneNumber.unique.phone_number,
-      gender: ['male', 'female'].sample,
-      email: Faker::Internet.email,
-      document: Faker::Kpop.girl_groups,
-      address: Faker::Address.full_address,
-      birthdate: Faker::Date.birthday(min_age: 18, max_age: 65)
+  10.times do
+    name = Faker::Name.name
+
+    Customer.create(
+      email: "#{name.split(' ').first}@gmail.com",
+      name: name,
+      phone: Faker::PhoneNumber.unique.cell_phone,
+      details: ''
     )
   end
 
-  Employee.first.update(services: [Service.first])
+  5.times do
+    services = Services.all
+    name = Faker::Name.name
+
+    Employee.create(
+      name: Faker::Name.unique.name,
+      phone: Faker::PhoneNumber.unique.cell_phone,
+      email: "#{name.split(' ').first}@luzdomundo.com.br",
+      document: Faker::IDNumber.unique.brazilian_citizen_number(formatted: true),
+      address: Faker::Address.unique.street_address,
+      birthdate: Faker::Date.birthday(min_age: 18, max_age: 50),
+      services: services.sample([1,2,3].sample)
+    )
+  end
 end
